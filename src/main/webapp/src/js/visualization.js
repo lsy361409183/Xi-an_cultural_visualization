648bc90a7fb66ddb2beafdbdb2ccdbf938bae60b
wl <wanglei@yeyisun.com>
2019-09-02 09:58:43 +0800

    更新可视化分布页面

--- src/main/webapp/src/js/visualization.js
+++ src/main/webapp/src/js/visualization.js
@@ -2,6 +2,6 @@ define(function(require, exports, module){
     var $ = require('../libs/jquery-3.3.1.min');
     var navTpl = require('../views/templates/navigation.tpl')
 
-    var test = $('#navbar');
+    var test = $('#nav-tpl');
     test.html(navTpl);
 });
