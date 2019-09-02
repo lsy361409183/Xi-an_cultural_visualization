27a28362757db929272cefe991b3b31cd97cfc51
lsy361409183 <361409183@qq.com>
2019-09-02 10:56:12 +0800

    加载地图

--- src/main/java/com/xiaancultural1/demo/controller/VisualCulturalController.java
+++ src/main/java/com/xiaancultural1/demo/controller/VisualCulturalController.java
@@ -1,18 +1,32 @@
 package com.xiaancultural1.demo.controller;
 
+import com.xiaancultural1.demo.pojo.MapData;
 import com.xiaancultural1.demo.pojo.visualBase;
 import com.xiaancultural1.demo.service.VisualCulturalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class VisualCulturalController {
     @Autowired
     private VisualCulturalService visualCulturalService;
+
+    //拿到geojson，加载地图
+    @RequestMapping("/getMapData")
+    @ResponseBody
+    public String selectJson(@RequestParam(value = "mapId",required = false)Integer mapId){
+        List<MapData> mapData = visualCulturalService.selectJson(mapId);
+        List<String> json=mapData.stream().map(MapData::getMapJson).collect(Collectors.toList());
+        //System.out.println(json.toString());
+        return json.toString();
+      // return visualCulturalService.selectJson(mapId);
+    };
     //查询出文地的基本信息：区域、类别、名称、位置、面积
     @RequestMapping("/getVisualData")
     @ResponseBody
--- src/main/java/com/xiaancultural1/demo/service/VisualCulturalService.java
+++ src/main/java/com/xiaancultural1/demo/service/VisualCulturalService.java
@@ -1,7 +1,9 @@
 package com.xiaancultural1.demo.service;
 
 import com.xiaancultural1.demo.mapper.VisualCulturalMapper;
+import com.xiaancultural1.demo.pojo.MapData;
 import com.xiaancultural1.demo.pojo.visualBase;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,11 @@ import java.util.List;
 public class VisualCulturalService {
     @Autowired
     private VisualCulturalMapper visualCulturalMapper;
+
+    //加载地图
+    public List<MapData> selectJson(Integer mapId){
+        return visualCulturalMapper.selectJson(mapId);
+    };
     //查询出文地的基本信息：区域、类别、名称、位置、面积
     public List<visualBase> selectAllInfo(){
         return visualCulturalMapper.selectAllInfo();
