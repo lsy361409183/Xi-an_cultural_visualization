19146c0ab7bcfcd280e3f906a96dd79d6c60d79d
lsy361409183 <361409183@qq.com>
2019-09-04 16:52:06 +0800

    lsy

--- src/main/webapp/src/js/visualization.js
+++ src/main/webapp/src/js/visualization.js
@@ -24,128 +24,41 @@ define(function(require, exports, module){
         setMapHeight();
     });
 
-
-    // 地图相关函数
-    var mapData = {},
-        pointData = [];
     // 请求地图geojson
-    function getMapGeoJson () {
+    function getGeoJson () {
         $.ajax({
             type:"get",
             url:"/getMapData",
-            async: false,
             data: {mapId: 1},
             dataType:"json",
             success:function(res){
-               mapData = res
-            }
-        });
-    }
-    function getPointData() {
-        $.ajax({
-            type: 'get',
-            url: '/getVisualData',
-            async: false,
-            success: function (res) {
-                pointData = res
-            }
-        })
-    }
-    function renderMap () {
-        var projection = new ol.proj.Projection({
-            code: 'EPSG:4326',
-            units: 'degrees'
-        });
-        var view = new ol.View({
-            projection: projection,
-            center:  [108.939621, 34.343147],
-            zoom:10,
-            maxZoom:25,
-            minZoom:5
-        });
-        var vectorLayer = new ol.layer.Vector({
-            source: new ol.source.Vector({
-                features: (new ol.format.GeoJSON()).readFeatures(mapData)
-            })
-        });
-
-        // 创建点
-        var tempPointArr = pointData.filter(function (item) {
-            return item.basePoint !== null;
-        });
-        var pointArr = tempPointArr.map(function (item) {
-            var pointCoordArr = item.basePoint.split(',');
-            return new ol.Feature(new ol.geom.Point([Number(pointCoordArr[0]),Number(pointCoordArr[1])]))
-        });
-        var pointSource = new ol.source.Vector({
-            features: pointArr
-        });
-
-        //聚合标注数据源
-        // var clusterSource = new ol.source.Cluster({
-        //     distance: 40,               //聚合的距离参数，即当标注间距离小于此值时进行聚合，单位是像素
-        //     source: pointSource         //聚合的数据源，即矢量要素数据源对象
-        // });
-        //加载聚合标注的矢量图层
-        // var styleCache = {};            //用于保存特定数量的聚合群的要素样式
-
-        var cultural_point = new ol.layer.Vector({
-            source: pointSource,
-            style: new ol.style.Style({
-                image: new ol.style.Circle({
-                    radius: 5,
-                    stroke: new ol.style.Stroke({
-                        color: '#fff'
-                    }),
-                    fill: new ol.style.Fill({
-                        color: '#FF0033'
+                var projection = new ol.proj.Projection({
+                    code: 'EPSG:4326',
+                    units: 'degrees'
+                });
+                var view = new ol.View({
+                    projection: projection,
+                    center:  [108.939621, 34.343147],
+                    zoom:10,
+                    maxZoom:25,
+                    minZoom:5
+                });
+                var vectorLayer = new ol.layer.Vector({
+                    source: new ol.source.Vector({
+                        features: (new ol.format.GeoJSON()).readFeatures(res)
                     })
+                });
+                var map = new ol.Map({
+                    layers: [
+                        vectorLayer
+                    ],
+                    target: 'visualization-map',
+                    view: view
                 })
-            })
-            // style: function (feature, resolution){
-            //     console.log('feature',feature);
-            //     // console.log('resolution',resolution);
-            //     var size = feature.get('features').length;          //获取该要素所在聚合群的要素数量
-            //     var style = styleCache[size];
-            //     // console.log('size',size);
-            //     if(!style){
-            //         style = [
-            //             new ol.style.Style({
-            //                 image: new ol.style.Circle({
-            //                     radius: 10,
-            //                     stroke: new ol.style.Stroke({
-            //                         color: '#fff'
-            //                     }),
-            //                     fill: new ol.style.Fill({
-            //                         color: '#33CC33'
-            //                     })
-            //                 }),
-            //                 text: new ol.style.Text({
-            //                     text: size.toString(),
-            //                     fill: new ol.style.Fill({
-            //                         color: '#fff'
-            //                     })
-            //                 })
-            //             })
-            //         ];
-            //         styleCache[size] = style;
-            //         // console.log('styleCache[size]',styleCache[size]);
-            //     }
-            //     return style;
-            // }
+            }
         });
-
-        var map = new ol.Map({
-            layers: [
-                vectorLayer
-            ],
-            target: 'visualization-map',
-            view: view
-        });
-        map.addLayer(cultural_point);
     }
+
     // 加载地图
-    getMapGeoJson();
-    getPointData();
-    renderMap();
+    getGeoJson();
 });
