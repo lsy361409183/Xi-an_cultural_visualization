581cc6fcdf704109cf515726bae0cceda0735e0c
MaybeJT <1317239583@qq.com>
2019-09-05 16:49:10 +0800

    bs

--- src/main/java/com/xiaancultural1/demo/controller/TableCulturalController.java
+++ src/main/java/com/xiaancultural1/demo/controller/TableCulturalController.java
@@ -5,37 +5,42 @@ import com.github.pagehelper.PageInfo;
 import com.xiaancultural1.demo.pojo.tableBase;
 import com.xiaancultural1.demo.service.TableCulturalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class TableCulturalController {
     @Autowired
     private TableCulturalService tableCulturalService;
 
     //根据页码分页，写死20条，全部记录
     @GetMapping("/select")
-    public PageInfo<tableBase> lists(@RequestParam int page){
+    @ResponseBody
+    public List<tableBase> lists(@RequestParam int page){
 //        设置分页规则
+       // PageHelper.clearPage();
         PageHelper.startPage(page,20);
 //        返回所有分页信息参数为查询所有记录的信息
         PageInfo<tableBase> pageInfo = new PageInfo<>(tableCulturalService.selectAllTableWithPage());
-        return pageInfo;
+        return pageInfo.getList();
+
+//        return tableCulturalService.selectAllTableWithPage();
     }
 
-    @GetMapping("/select1")
-    public PageInfo<tableBase> districtLists(@RequestParam String district,@RequestParam int page){
-        PageHelper.startPage(page,20);
-        PageInfo<tableBase> pageInfo =new PageInfo<>(tableCulturalService.selectAllTableByDistrictWithPage());
-        return pageInfo;
-    }
-    @GetMapping("/select2")
-    public PageInfo<tableBase> classificationLists(@RequestParam String classification,@RequestParam int page){
-        PageHelper.startPage(page,20);
-        PageInfo<tableBase> pageInfo =new PageInfo<>(tableCulturalService.selectAllTableByClassificationtWithPage());
-        return pageInfo;
-    }
+//    @GetMapping("/select1")
+//    public PageInfo<tableBase> districtLists(@RequestParam String district,@RequestParam int page){
+//        PageHelper.startPage(page,20);
+//        PageInfo<tableBase> pageInfo =new PageInfo<>(tableCulturalService.selectAllTableByDistrictWithPage());
+//        return pageInfo;
+//    }
+//    @GetMapping("/select2")
+//    public PageInfo<tableBase> classificationLists(@RequestParam String classification,@RequestParam int page){
+//        PageHelper.startPage(page,20);
+//        PageInfo<tableBase> pageInfo =new PageInfo<>(tableCulturalService.selectAllTableByClassificationtWithPage());
+//        return pageInfo;
+//    }
 //    //传页码以及每页条数，全部记录
 //    @GetMapping("/select1")
 //    public List<tableBase> paraList(@RequestParam(defaultValue = "1") int p,
