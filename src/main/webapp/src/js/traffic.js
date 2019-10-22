425bdb0ec12c33c075031df3cc1ab8e74db73fd6
lsy361409183 <361409183@qq.com>
2019-10-22 09:15:30 +0800

    交通可达性222222222222syhahaha

--- src/main/webapp/src/js/traffic.js
+++ src/main/webapp/src/js/traffic.js
@@ -44,7 +44,7 @@ define(function(require, exports, module) {
         var map_container_width =$('#map_container').width();
         map.css('width',(map_container_width-20)/2+'px');
         map.css('height',(map_container_height/2)+'px');
-
+        console.log(map_container_height)
         //第二张图
         var map2=$('#wrapper2');
         map2.css('width',(map_container_width-20)/2+'px');
@@ -57,10 +57,18 @@ define(function(require, exports, module) {
         var map4=$('#wrapper4');
         map4.css('width',(map_container_width-20)/2+'px');
         map4.css('height', (map_container_height/2-20)+'px');
+        //修改右边长方形的高度
+        var ss = $('#square');
+        ss.css('height',(clientHeight-nav_height)+'px');
 
     }
+    $("#drawing").click(function(){
+        Drawing();
+    });
 
 
+function Drawing() {
+        console.log(1234)
 //获取西安站的数据
     $.ajax({
         type: 'post',
@@ -79,7 +87,7 @@ define(function(require, exports, module) {
             var lat = data.map(function (item) {
                 return item.baseLatitude
             })
-            console.log("可达性："+count+"经度："+lon+"纬度"+lat)
+            console.log("可达性：" + count + "经度：" + lon + "纬度" + lat)
 
 
             //引入热力图
@@ -90,16 +98,16 @@ define(function(require, exports, module) {
                 // {"lng":108.841037,"lat":34.302558,"count":10068},
                 // {"lng":109.011909,"lat":34.325429,"count":11090},
                 // {"lng":109.056604,"lat":34.395023,"count":15040},
-              data.map(function (item) {
-                return{
-                   "lng":item.baseLongitude === null? 0 :item.baseLongitude,
-                    "lat":item.baseLatitude === null? 0 :item.baseLatitude,
-                    "count":item.accessibility === null? 0 :item.accessibility
-                }
-              });
+                data.map(function (item) {
+                    return {
+                        "lng": item.baseLongitude === null ? 0 : item.baseLongitude,
+                        "lat": item.baseLatitude === null ? 0 : item.baseLatitude,
+                        "count": item.accessibility === null ? 0 : item.accessibility
+                    }
+                });
 
             var heatmap;
-            map.plugin(["AMap.Heatmap"], function() {
+            map.plugin(["AMap.Heatmap"], function () {
                 //初始化heatmap对象
                 heatmap = new AMap.Heatmap(map, {
                     radius: 50, //给定半径(热力图显示的圆点的半径)
@@ -111,7 +119,7 @@ define(function(require, exports, module) {
                      0.9: '#ffea00',
                      1.0: 'red'
                      }*/
-                    gradient:{          //热力图的颜色渐变区间。   {JSON}:key 插值的位置, 0-1;  value颜色值
+                    gradient: {          //热力图的颜色渐变区间。   {JSON}:key 插值的位置, 0-1;  value颜色值
                         0.5: '#50a3ba',
                         0.65: '#9DB578',
                         0.7: '#eac736',
@@ -126,12 +134,167 @@ define(function(require, exports, module) {
                 });
             });
 
-
-
-
         }
     })
 
+    //获取西安南站的数据
+    $.ajax({
+        type: 'post',
+        url: '/getTrafficData1',
+        dataType: 'json',
+        success: function (data) {
+            // var point = data.map(function (item) {
+            //     return item.basePoint
+            // });
+            // var count = data.map(function (item) {
+            //     return item.accessibility
+            // })
+            // var lon = data.map(function (item) {
+            //     return item.baseLongitude
+            // })
+            // var lat = data.map(function (item) {
+            //     return item.baseLatitude
+            // })
+            //console.log("可达性："+count+"经度："+lon+"纬度"+lat)
 
+
+            //引入热力图
+            // 坐标点
+            var points =
+                data.map(function (item) {
+                    return {
+                        "lng": item.baseLongitude === null ? 0 : item.baseLongitude,
+                        "lat": item.baseLatitude === null ? 0 : item.baseLatitude,
+                        "count": item.accessibility === null ? 0 : item.accessibility
+                    }
+                });
+
+            var heatmap;
+            map2.plugin(["AMap.Heatmap"], function () {
+                //初始化heatmap对象
+                heatmap = new AMap.Heatmap(map2, {
+                    radius: 50, //给定半径(热力图显示的圆点的半径)
+                    opacity: [0, 0.8],
+                    /*,gradient:{
+                     0.5: 'blue',
+                     0.65: 'rgb(117,211,248)',
+                     0.7: 'rgb(0, 255, 0)',
+                     0.9: '#ffea00',
+                     1.0: 'red'
+                     }*/
+                    gradient: {          //热力图的颜色渐变区间。   {JSON}:key 插值的位置, 0-1;  value颜色值
+                        0.5: '#50a3ba',
+                        0.65: '#9DB578',
+                        0.7: '#eac736',
+                        0.9: '#E28B40',
+                        1.0: '#d94e5d'
+                    }
+                });
+                //设置数据集：该数据为重庆景点数据
+                heatmap.setDataSet({
+                    data: points,
+                    max: 20000
+                });
+            });
+
+        }
+    })
+    //获取西安北站的数据
+    $.ajax({
+        type: 'post',
+        url: '/getTrafficData2',
+        dataType: 'json',
+        success: function (data) {
+
+            //引入热力图
+            // 坐标点
+            var points =
+                data.map(function (item) {
+                    return {
+                        "lng": item.baseLongitude === null ? 0 : item.baseLongitude,
+                        "lat": item.baseLatitude === null ? 0 : item.baseLatitude,
+                        "count": item.accessibility === null ? 0 : item.accessibility
+                    }
+                });
+
+            var heatmap;
+            map3.plugin(["AMap.Heatmap"], function () {
+                //初始化heatmap对象
+                heatmap = new AMap.Heatmap(map3, {
+                    radius: 50, //给定半径(热力图显示的圆点的半径)
+                    opacity: [0, 0.8],
+                    /*,gradient:{
+                     0.5: 'blue',
+                     0.65: 'rgb(117,211,248)',
+                     0.7: 'rgb(0, 255, 0)',
+                     0.9: '#ffea00',
+                     1.0: 'red'
+                     }*/
+                    gradient: {          //热力图的颜色渐变区间。   {JSON}:key 插值的位置, 0-1;  value颜色值
+                        0.5: '#50a3ba',
+                        0.65: '#9DB578',
+                        0.7: '#eac736',
+                        0.9: '#E28B40',
+                        1.0: '#d94e5d'
+                    }
+                });
+                //设置数据集：该数据为重庆景点数据
+                heatmap.setDataSet({
+                    data: points,
+                    max: 20000
+                });
+            });
+
+        }
+    })
+//获取机场的数据
+    $.ajax({
+        type: 'post',
+        url: '/getTrafficData3',
+        dataType: 'json',
+        success: function (data) {
+
+            //引入热力图
+            // 坐标点
+            var points =
+                data.map(function (item) {
+                    return {
+                        "lng": item.baseLongitude === null ? 0 : item.baseLongitude,
+                        "lat": item.baseLatitude === null ? 0 : item.baseLatitude,
+                        "count": item.accessibility === null ? 0 : item.accessibility
+                    }
+                });
+
+            var heatmap;
+            map4.plugin(["AMap.Heatmap"], function () {
+                //初始化heatmap对象
+                heatmap = new AMap.Heatmap(map4, {
+                    radius: 50, //给定半径(热力图显示的圆点的半径)
+                    opacity: [0, 0.8],
+                    /*,gradient:{
+                     0.5: 'blue',
+                     0.65: 'rgb(117,211,248)',
+                     0.7: 'rgb(0, 255, 0)',
+                     0.9: '#ffea00',
+                     1.0: 'red'
+                     }*/
+                    gradient: {          //热力图的颜色渐变区间。   {JSON}:key 插值的位置, 0-1;  value颜色值
+                        0.5: '#50a3ba',
+                        0.65: '#9DB578',
+                        0.7: '#eac736',
+                        0.9: '#E28B40',
+                        1.0: '#d94e5d'
+                    }
+                });
+                //设置数据集：该数据为重庆景点数据
+                heatmap.setDataSet({
+                    data: points,
+                    max: 20000
+                });
+            });
+
+        }
+    })
+}
 
 })
