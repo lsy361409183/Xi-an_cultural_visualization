8a1836639c1ffc23e9aa77fee54a600d141f7321
wl <wanglei@yeyisun.com>
2019-11-04 14:59:25 +0800

    更新-KDE

---
+++ src/main/java/com/xiaancultural1/demo/utils/kde.py
@@ -0,0 +1,25 @@
+from scipy import stats
+import numpy as np
+import matplotlib.pyplot as plt
+
+def adder(a,b):
+    m1 = np.array(a)
+    m2 = np.array(b)
+    xmin = m1.min()
+    xmax = m1.max()
+    ymin = m2.min()
+    ymax = m2.max()
+
+    X, Y = np.mgrid[xmin:xmax:100j, ymin:ymax:100j]
+    positions = np.vstack([X.ravel(), Y.ravel()])
+    values = np.vstack([m1, m2])
+    kernel = stats.gaussian_kde(values)
+    Z = np.reshape(kernel(positions).T, X.shape)
+
+    fig, ax = plt.subplots()
+    ax.imshow(np.rot90(Z), cmap=plt.cm.gist_earth_r,
+              extent=[xmin, xmax, ymin, ymax])
+    ax.plot(m1, m2, 'k.', markersize=2)
+    ax.set_xlim([xmin, xmax])
+    ax.set_ylim([ymin, ymax])
+    plt.show()
---
+++ src/main/java/com/xiaancultural1/demo/utils/test.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+import sys
+
+
+def adder(a, b):
+    print(a,b)
+
+if __name__ == '__main__':
+    for i in range(1, len(sys.argv)):
+        print(sys.argv[1:])
